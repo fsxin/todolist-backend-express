e1f74bc0b0248146e6614e24499009203ca5fade
Felix.Xu <Felix.Xu@avepoint.com>
2021-06-28 10:32:07 +0800

    uninstall body-parser

--- app.ts
+++ app.ts
@@ -1,11 +1,10 @@
-import * as bodyParser from 'body-parser'; // 引入body-parser模块
 import * as express from 'express'; // 引入express模块
 import * as cors from 'cors'; // 引入cors模块
 import routes from './routes'; //导入自定义路由文件，创建模块化路由
 const app = express();
 
-app.use(bodyParser.json()); // 解析json数据格式
-app.use(bodyParser.urlencoded({ extended: true })); // 解析form表单提交的数据application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));// 解析form表单提交的数据application/x-www-form-urlencoded 
+app.use(express.json()); // 解析json数据格式
 
 app.use(cors()); // 注入cors模块解决跨域
 
